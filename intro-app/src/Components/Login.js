f40756a735a84f044570dc015591988be84e5705
Nataorjuela <nataorjuela62@gmail.com>
2022-09-21 22:53:07 -0500

    login button

--- intro-app/src/Components/Login.js
+++ intro-app/src/Components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Login.css';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 //Create base layout for the login ( email box, password box, login button, basic styling ),
 // you should use a UI Library like mui or ant.
 
@@ -10,6 +11,7 @@ function Login() {
         <h1>LOGIN</h1>
       <TextField id="outlined-basic" label="Email" variant="outlined" />
       <TextField id="outlined-basic" label="Password" variant="outlined" />
+      <Button variant="contained">Login</Button>
     </div>
   );
 }
